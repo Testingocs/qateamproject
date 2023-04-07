05621265ca0487a64cf156de174d0512d84ae241
macminir01 <macminir01@localhost>
2023-04-07 15:47:59 +0530

    Flowfullcommit

---
+++ Maven/maven/src/main/java/mavenid/maven/App.java
@@ -0,0 +1,13 @@
+package mavenid.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
